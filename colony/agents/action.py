293e7554e8c588ed0b9fc2ee5d8bac4144fddc9a
Berat Emir Mutlu <beratemutlu@gmail.com>
2026-05-10 01:08:31 +0200

    feat: expand ant movement to 8-directional neighbourhood

--- colony/agents/action.py
+++ colony/agents/action.py
@@ -8,10 +8,14 @@ class ActionType(Enum):
     IDLE = 4
 
 class Direction(Enum):
-    NORTH = (0, -1)
+    NORTH = (0, 1)
+    NORTHEAST = (1, 1)
     EAST  = (1, 0)
-    SOUTH = (0, 1)
+    SOUTHEAST = (1, -1)
+    SOUTH = (0, -1)
+    SOUTHWEST = (-1, -1)
     WEST  = (-1, 0)
+    NORTHWEST = (-1, 1)
 
     def apply(self, pos: tuple[int, int]) -> tuple[int, int]:
         return pos[0] + self.value[0], pos[1] + self.value[1]
